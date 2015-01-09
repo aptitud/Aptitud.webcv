98b92d84d687588c01fa711f2f370fc16ab9efa3
Marcus Nylander <marcus.nylander@aptitud.se>
2015-01-09 11:22:36 +0100

    Changed to select-ui search controller instead of home made

--- web-cv-app/src/main/webapp/app/app.js
+++ web-cv-app/src/main/webapp/app/app.js
@@ -1,4 +1,4 @@
-angular.module('configuration', [])
+angular.module('configuration', ['ui.select'])
        //.constant('API_END_POINT','http://afternoon-river-5325.herokuapp.com')
 	   .constant('API_END_POINT','http://localhost:8000')
        .constant('CLIENT_ID','57103926862-ktsb4791lhv5fp42tista6jhocosc3rg.apps.googleusercontent.com');
